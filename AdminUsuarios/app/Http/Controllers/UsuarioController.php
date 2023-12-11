3f77db3062f49d5fc85fd82fc0ef1bcefd712f10
nicky <nickylds1958@gmail.com>
2023-12-11 16:58:29 -0600

    laravel

---
+++ AdminUsuarios/app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Usuario;
+use Illuminate\Http\Request;
+
+class UsuarioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Usuario::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $usuario = new Usuario();
+        $usuario->nombre = $request->nombre;
+        $usuario->apellido = $request->apellido;
+        $usuario->correo = $request->correo;
+        $usuario->fecha = $request->fecha;
+        $usuario->save();
+        return "el vendedor se guardo correctamente";
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        $usuario = Usuario::find($id);
+        return $usuario;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Usuario $usuario)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $id)
+    {
+        $usuario = Usuario::find($id);
+
+        $usuario->nombre = $request->nombre;
+        $usuario->apellido = $request->apellido;
+        $usuario->correo = $request->correo;
+        $usuario->fecha = $request->fecha;
+        $usuario->save();
+        return "actualizado corectamente";
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+
+    {
+        $usuario = Usuario::find($id);
+        $usuario->delete();
+        return "eliminado ";
+    }
+}
